be64ee08a2d95f41da4e53da39243d1d8389e5f0
Iacopino Thomas <tiacopino@eurac.edu>
2020-05-20 18:14:51 +0200

    Change: update next

--- components/post-header.js
+++ components/post-header.js
@@ -1,11 +1,14 @@
+import Avatar from '../components/avatar'
+import Date from '../components/date'
 import CoverImage from '../components/cover-image'
 import PostTitle from '../components/post-title'
 
-export default function PostHeader({ title, coverImage }) {
+export default function PostHeader({ title, coverImage, date, author }) {
   return (
     <>
       <PostTitle>{title}</PostTitle>
       <div className="hidden md:block md:mb-12">
+        {author.name}
       </div>
       <div className="mb-8 md:mb-16 -mx-5 sm:mx-0">
         <CoverImage
@@ -15,8 +18,9 @@ export default function PostHeader({ title, coverImage }) {
       </div>
       <div className="max-w-2xl mx-auto">
         <div className="block md:hidden mb-6">
-        </div>
+          </div>
         <div className="mb-6 text-lg">
+        {author.name}NAME
         </div>
       </div>
     </>
