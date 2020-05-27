ae83b9a62867c77a196240958e5d2c49120df802
Iacopino Thomas <tiacopino@eurac.edu>
2020-05-27 18:23:13 +0200

    Change: add /videoposts/slug route to test videos and performance

---
+++ pages/videoposts/[slug].js
@@ -0,0 +1,83 @@
+import { useRouter } from 'next/router'
+import ErrorPage from 'next/error'
+import Container from '../../components/container'
+import PostBody from '../../components/post-body'
+import MoreStories from '../../components/more-stories'
+import Header from '../../components/header'
+import PostHeader from '../../components/post-header'
+import SectionSeparator from '../../components/section-separator'
+import Layout from '../../components/layout'
+import { getAllPostsWithSlug, getPostAndMorePosts } from '../../lib/api'
+import Head from 'next/head'
+import { CMS_NAME } from '../../lib/constants'
+import markdownToHtml from '../../lib/markdownToHtml'
+import VideoPlayer from '../../components/video-player';
+
+
+export default function Story({ story, morePosts, preview }) {
+  const router = useRouter()
+  if (!router.isFallback && !story?.slug) {
+    return <ErrorPage statusCode={404} />
+  }
+  return (
+    <Layout preview={preview}>
+      <Container>
+        <Header />
+        {router.isFallback ? (
+          <div>loading</div>
+        ) : (
+          <>
+            <VideoPlayer
+              autoPlay
+              muted
+              loop
+              playsinline
+              id="videoid"
+              className="object-cover w-full"
+              poster="https://image.mux.com/JW8SSYrt1ZhC9022T3pephvqHq2uATuDP/thumbnail.jpg"
+              src="https://stream.mux.com/JW8SSYrt1ZhC9022T3pephvqHq2uATuDP.m3u8"
+            />
+            <article>
+              <Head>
+                <title>
+                  {story.title} | Next.js Blog Example with {CMS_NAME}
+                </title>
+                <meta property="og:image" content={story.ogImage.url} />
+              </Head>
+              <PostHeader
+                title={story.title}
+                coverImage={story.coverImage}
+                date={story._firstPublishedAt}
+                author={story.author}
+              />
+              <p>Abstract:<br/> {story.abstract}</p>
+              <PostBody content={story.content} />
+            </article>
+          </>
+        )}
+      </Container>
+    </Layout>
+  )
+}
+
+export async function getStaticProps({ params, preview = false }) {
+  const data = await getPostAndMorePosts(params.slug, preview)
+
+  return {
+    props: {
+      preview,
+      story: {
+        ...data?.story,
+      },
+    },
+    unstable_revalidate: 1
+  }
+}
+
+export async function getStaticPaths() {
+  const allStories = await getAllPostsWithSlug()
+  return {
+    paths: allStories?.map(story => `/posts/${story.slug}`) || [],
+    fallback: true,
+  }
+}
