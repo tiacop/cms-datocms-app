5167312f71a2555646913b016435b288ccf2e013
tiaco <thomas.iacopino@gmail.com>
2020-07-23 10:25:05 +0200

    Add webvitals

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,12 @@
 import '../styles/index.css'
 
+
+// Will be called once for every metric that has to be reported.
+export function reportWebVitals(metric) {
+  // These metrics can be sent to any analytics service
+  console.log(metric)
+}
+
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
